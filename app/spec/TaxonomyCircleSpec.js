778112be3de3226cf5f10b933107af64553a7061
Jon Lebensold <jon@lebensold.ca>
2011-06-14 22:33:13 +0300

    got the recursive thing working... now working on drawing

--- app/spec/TaxonomyCircleSpec.js
+++ app/spec/TaxonomyCircleSpec.js
@@ -28,44 +28,50 @@ describe("TaxonomyCircle" , function() {
           ];
 
     });
-/*    it ("should handle taxonomies recursively",function()
-    {
-        console.log('a');
-        taxonomyCircle.update({Cars:[{'2 door':["sports car", "Fiat 500"]}, {Van:["Toyota thing", "VW Bus"]}]});
-        expect(taxonomyCircle.getRingDepth(3)).toEqual([{depth:1,value:"Cars"}]);
-
-    }); */
-   
-    it("should get taxonomy depth", function() 
-    {
-        taxonomyCircle.update(taxonomy);
-        expect(taxonomyCircle.taxonomyDepth()).toEqual(4);
-    });
-/*    it("should get ring depth", function()
-    {
-        taxonomyCircle.update(taxonomy);
-        expect(taxonomyCircle.getRingDepth(4)).toEqual([ 
-        {
-            "Ontario": ["Toronto"]
-            },
-
-            {
-            "Quebec": ["Sherbrooke","Montreal"]
-            }
-        ]);
-    });
-*/
-    it("should have a default x / y / size", function()
-    {
-        expect(taxonomyCircle.data.x).toBeDefined();
-        expect(taxonomyCircle.data.y).toBeDefined();
-        expect(taxonomyCircle.data.size).toBeDefined();
-    });
     it("should be able to be drawn to paper", function()
     {     
         taxonomyCircle.update(taxonomy);
         taxonomyCircle.draw(paper);
         expect(taxonomyCircle.drawn()).toBeTruthy();
+    });
+    return;
+    it ("should handle taxonomies recursively",function()
+    {
+        taxonomyCircle.update({"Cars":[{'2 door':["sports car", "Fiat 500"]}, {'Van':["Toyota thing", "VW Bus"]}]});
+        expect(taxonomyCircle.getRingDepth(3)).toEqual([
+         { depth : 3, value : 'sports car' }, 
+         { depth : 3, value : 'Fiat 500' }, 
+         { depth : 3, value : 'Toyota thing' }, 
+         { depth : 3, value : 'VW Bus' }     
+        ]);
+    });
+    it ("should get flattened taxonomies recursively",function()
+    {
+        var ar = {"Cars":[
+                    {'2 door':["sports car", "Fiat 500"]}, 
+                    {"Van":["Toyota thing", "VW Bus"]}
+                  ]};
+        expect(taxonomyCircle.getFlattenedTaxonomy(ar, 1, [])).toEqual(
+        [{ depth : 1, value : 'Cars' }, 
+         { depth : 2, value : '2 door' }, 
+         { depth : 3, value : 'sports car' }, 
+         { depth : 3, value : 'Fiat 500' }, 
+         { depth : 2, value : 'Van' }, 
+         { depth : 3, value : 'Toyota thing' }, 
+         { depth : 3, value : 'VW Bus' } 
+        ]);
+        
+    });
+    it("should get taxonomy depth", function() 
+    {
+        taxonomyCircle.update(taxonomy);
+        expect(taxonomyCircle.taxonomyDepth()).toEqual(4);
+    });
+    it("should have a default x / y / size", function()
+    {
+        expect(taxonomyCircle.data.x).toBeDefined();
+        expect(taxonomyCircle.data.y).toBeDefined();
+        expect(taxonomyCircle.data.size).toBeDefined();
     });
      
      
