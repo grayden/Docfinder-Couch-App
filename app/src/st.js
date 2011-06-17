f73ea0a3f024ea638db26136476d65ff5bc692e6
Jon Lebensold <jon@lebensold.ca>
2011-06-18 00:41:27 +0300

    ditching the HyperTree for now... apparently divs also work

--- app/src/st.js
+++ app/src/st.js
@@ -203,7 +203,7 @@ function init() {
                     
             }
     });
-
+/*
     var container = new $jit.Hypertree(
     {
         //useCanvas : container.canvas,
@@ -232,55 +232,39 @@ function init() {
             offsetY: -90
         }
     });
-}
+    */
+
 
 var bursts = {};
 
 
-$jit.Hypertree.Plot.NodeTypes.implement({  
-	'adv-rect': {  
-		'render': function(node, canvas) {  
-                                                    console.log(this);
-                                                    return;
-			var width = node.getData('width'),  
-				height = node.getData('height'),  
-				pos = this.getAlignedPos(node.pos.getc(true), width, height),  
-				posX = pos.x + width/2,
-				posY = pos.y + height/2,
-				radius = node.getCanvasStyle("radius"),
-				RoundRect = {
-					'render': function(type, pos, width, height, radius, canvas) {
-						var ctx = canvas.getCtx(),
-							x = pos.x - width/2,
-							y = pos.y - height/2;
-	
-						ctx.beginPath();  
-						ctx.moveTo(x,y+radius);  
-						ctx.lineTo(x,y+height-radius);  
-						ctx.quadraticCurveTo(x,y+height,x+radius,y+height);  
-						ctx.lineTo(x+width-radius,y+height);  
-						ctx.quadraticCurveTo(x+width,y+height,x+width,y+height-radius);  
-						ctx.lineTo(x+width,y+radius);  
-						ctx.quadraticCurveTo(x+width,y,x+width-radius,y);  
-						ctx.lineTo(x+radius,y);  
-						ctx.quadraticCurveTo(x,y,x,y+radius);  
-						ctx[type]();
-					}	
-				};
+var sb = new $jit.Sunburst({
+        injectInto: "foobar",
 
-			if (radius > 0) {
-				RoundRect.render('fill', {x: posX, y: posY}, width, height, radius, canvas);
-				RoundRect.render('stroke', {x: posX, y: posY}, width, height, radius, canvas);
-			} else {
-				this.nodeHelper.rectangle.render('fill', {x: posX, y: posY}, width, height, canvas);  
-				this.nodeHelper.rectangle.render('stroke', {x: posX, y: posY}, width, height, canvas);
-			}  
-		},
-		'contains': function(node, pos) {
-			var width = node.getData('width'),
-				height = node.getData('height'),
-				npos = this.getAlignedPos(node.pos.getc(true), width, height);
-			return this.nodeHelper.rectangle.contains({x:npos.x+width/2, y:npos.y+height/2}, pos, width, height);
-		}
-	}
-});
+        levelDistance: 40,
+        Node: {
+          overridable: true,
+          type: 'gradient-multipie'
+        },
+        Label: {
+          type: 'Native'
+        },
+        NodeStyles: {
+          enable: true,
+          type: 'Native',
+          stylesClick: {
+            'color': '#33dddd'
+          },
+          stylesHover: {
+            'color': '#dd3333'
+          }
+        },
+        clearCanvas: false  
+    });
+
+//load JSON data.
+sb.loadJSON(rootSunburstJSON.data.sunburst);
+sb.compute();
+sb.refresh();
+
+}
