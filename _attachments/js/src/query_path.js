13bce301ad63c8c4d8891978a9e5a287d4932526
Grayden Smith <grayden@tech-vision.ca>
2011-06-22 10:36:24 +0300

    Save new path format

--- _attachments/js/src/query_path.js
+++ _attachments/js/src/query_path.js
@@ -31,9 +31,8 @@ path_p.prototype.save_object = function (view, specs)
     return this.launch[count -1];
 }
 
-path_p.prototype.set_directions = function(path, starting_type, ending_type, starting_property, starting_key)
+path_p.prototype.set_directions = function(starting_type, ending_type, starting_property, starting_key)
 {
-    this.path = path;
     this.starting_type = starting_type;
     this.ending_type = ending_type;
     this.starting_property = starting_property;
@@ -42,7 +41,7 @@ path_p.prototype.set_directions = function(path, starting_type, ending_type, sta
 
 path_p.prototype.test_query_got_rows = function(to_test)
 {
-    if (to_test.rows) {return true;}
+    if (to_test.rows && to_test.rows.length > 0) {return true;}
     return false;
 }
 
@@ -52,12 +51,20 @@ path_p.prototype.determine_needed_view = function (type, property)
 {
     if (type == "country" && (property == "name" || property == "country.name"))
         return "use_country_name";
+    
+    if ((type == "country" || type == "city") && (property == "country.code" || property == "code"))
+        return "use_country_code";
+    
+    return "no_view";
 }
 
 path_p.prototype.test_starting_point = function()
 {
+    var view = this.determine_needed_view(this.starting_type, this.starting_property);
+    
+    console.log(this.couch_query(view,'key=["' + this.starting_key +'","' + this.starting_property + '"]'));
     return this.test_query_got_rows(
-    this.couch_query(this.determine_needed_view(this.starting_type, this.starting_property),'key=["' + this.starting_key +'","' + this.starting_property + '"]')
+    this.couch_query(view,'key=["' + this.starting_key +'","' + this.starting_property + '"]')
     );
 }
 
@@ -68,13 +75,73 @@ path_p.prototype.console_show_couch_query = function (view, specs)
 
 var path = new path_p;
 
+// V2
+var test_path = {
+    start_type: "county",
+    end_type: "dockey",
+    start_property: "name",
+    start_value: " Canada",
+    // Read like this: Country can link to city because city has a country.code 
+    // which matches the code of country
+    steps: {
+        country: [["city", "country.code", "code"]],
+        city: [["dockey", "city.name", "name"]]
+        }
+    };
+    
+path.set_directions("country", "dockey", "name", "Canada");
+console.log(path.test_starting_point());
+
+/*
+ V1
 var test_schema_array = [
-    // Read like this: Country can link to city because it has a country.code 
+    // Read like this: Country can link to city because city has a country.code 
     // which matches the code of country
     {country: [["city", "country.code", "code"]]},
     {city: [["country", "code", "country.code"], ["dockey", "city.name", "name"]]},
     {dockey: [["city", "name", "city.name"]]}
 ];
-path.set_directions(test_schema_array, "country", "dockey", "name", "Canada");
-console.log(path.test_starting_point());
-path.console_show_couch_query("use_country_name",'key=["Canada","country"]');
+*/
+
+
+
+
+/***** DEMO STUFF *****/
+
+var foo = function(listen)
+{
+    setTimeout(function()
+    {
+        $.get("/" +"docfinder/_design/docs/_view" + "/use_country_code",
+            function(resp)
+            {
+                listen.hasComeBack(resp); // dispatcher
+                foo(listen);
+            }
+        );
+
+    },3000);
+    
+};
+myListener = function(){
+    this.responses = [];
+};
+
+myListener.prototype.hasComeBack = function(resp)
+{
+    this.responses.push (resp);
+    console.log("im back: " + resp.length);
+}
+var m;
+$(function()
+{
+   //m = new myListener();
+   //foo(m);
+   $('.hello').live('click',function()
+    {
+        $("body").append("<a class='hello' href='#'>hello!</a>");
+    });
+
+
+    $("body").append("<a class='hello' href='#'>hello!</a>");
+});
